b1b074eba59b874c647acf25b86b386ef313db55
Ramdevsinh <ramdevsinh.vrinsoft@gmail.com>
2018-05-15 17:36:27 +0530

    Final Commite

---
+++ BlinkingLabel/Classes/BlinkingLabel.swift
@@ -0,0 +1,20 @@
+//
+//  BlinkingLabel.swift
+//  BlinkingLabel
+//
+//  Created by Vishal Gohil on 15/05/18.
+//
+
+import UIKit
+
+class BlinkingLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
